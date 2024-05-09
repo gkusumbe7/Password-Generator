a941f512a6660654c3572bdb9f89b01542f54139
Ganesh Kusumbe <gkusumbe7@gmail.com>
2024-05-10 01:49:53 +0530

    added code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import "./App.css";
+import { useState } from "react";
+
+
 function App() {
+  const [passwordGenerateLength , setPasswordGenerateLength ] = useState(0);
+  const [checkUpper , setCheckUpper ] = useState("");
+  const [checkLower , setCheckLower ] = useState("");
+  const [checkNumber , setCheckNumber ] = useState("");
+  const [checkSymbol , setCheckSymbol ] = useState("");
+  console.log(checkLower,checkNumber,checkSymbol,checkUpper);
+
+  const[generatePassword , setGeneratePassword] = useState(); 
+
+  // console.log(typeof passwordGenerateLength);
   return (
     <>
       <main className="flex text-center justify-center p-20 ">
-        <div className="bg-blue-200 flex flex-col gap-5  p-10 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
-          <p className="text-center font-serif font-bold text-4xl">
+        <div className="bg-cyan-200 flex flex-col gap-5  p-10 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+          <p className="text-center font-serif font-bold text-4xl drop-shadow-lg	">
             Password Generator
           </p>
 
@@ -16,6 +28,7 @@ function App() {
               type="text"
               className="px-1 ml-2 w-14 rounded-sm border border-black"
               name="password-length"
+              onChange={(e)=>setPasswordGenerateLength(e.target.value)}
             />
           </div>
 
@@ -25,23 +38,30 @@ function App() {
             </h2>
             <div className="flex flex-col m-2">
               <label className="text-md font-semibold">
-                {" "}
-                <input type="checkbox" name="" id="" />
-                &nbsp; Include upper case{" "}
+                <input type="checkbox" name="" id="" 
+                onChange={(e)=>setCheckUpper(e.target.checked)}
+                />
+                &nbsp; Include upper case
               </label>
               <label className="text-md font-semibold">
                 {" "}
-                <input type="checkbox" name="" id="" />
+                <input type="checkbox" name="" id="" 
+                onChange={(e)=>setCheckLower(e.target.checked)}
+                />
                 &nbsp; Include lower case
               </label>
               <label className="text-md font-semibold">
                 {" "}
-                <input type="checkbox" name="" id="" />
+                <input type="checkbox" name="" id="" 
+                onChange={(e)=>setCheckNumber(e.target.checked)}
+                />
                 &nbsp; Include numbers
               </label>
               <label className="text-md font-semibold">
                 {" "}
-                <input type="checkbox" name="" id="" />
+                <input type="checkbox" name="" id="" 
+                onChange={(e)=>setCheckSymbol(e.target.checked)}
+                />
                 &nbsp; Include symbols
               </label>
             </div>
@@ -50,14 +70,15 @@ function App() {
             <input
               type="text"
               className="py-1 px-1 border border-black rounded-md "
+              value={setGeneratePassword}
             ></input>
-            <button type="button" class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg mx-2 text-sm px-5 py-2 text-center me-2 mb-2">Copy </button>
+            <button type="button" className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg mx-2 text-sm px-5 py-2 text-center me-2 mb-2">Copy </button>
 
           </div>
          
           <button
             type="button"
-            class="ml-24 w-fit  text-white bg-gradient-to-r from-green-600 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-1 focus:outline-none focus:ring-black dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-md px-3 py-2.5 text-center me-2 mb-2"
+            className="ml-24 w-fit  text-white bg-gradient-to-r from-green-600 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-1 focus:outline-none focus:ring-black dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-md px-3 py-2.5 text-center me-2 mb-2"
           >
             Generate Password
           </button>
