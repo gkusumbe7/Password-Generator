df47e928e648bd29bdf619a75ddb20953ad4c589
Ganesh Kusumbe <gkusumbe7@gmail.com>
2024-05-10 00:04:27 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,33 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <main className="flex text-center justify-center p-20 ">
+
+              <div className="bg-cyan-200 flex flex-col gap-5 border border-black p-5 shadow-xl"> 
+                  <p className="text-center font-serif font-bold text-3xl">Password Generator</p>
+                  
+
+                  <div className="text-center">
+                      <label className="font-semibold text-md">Enter Password Length :</label>
+                      <input type="number" className="ml-2 w-14 rounded-sm border border-black" placeholder="Enter Length" name="password-length" />
+                  </div>
+
+                  <div className=" text-left ml-10">
+                    <h2 className="text-md font-semibold mb-2">Choose Password Generate Types:</h2>
+                    <div className="flex flex-col">
+                    <label > <input type="checkbox" name="" id="" />&nbsp; Include upper case</label>
+                    <label > <input type="checkbox" name="" id="" />&nbsp; Include lower case</label>
+                    <label > <input type="checkbox" name="" id="" />&nbsp; Include numbers</label>
+                    <label > <input type="checkbox" name="" id="" />&nbsp; Include symbols</label>
+                    </div>
+                    </div>
+                  <div className="ml-2">
+                    <input type="text" className="border border-black rounded-md "></input>
+                    <button className="border border-black rounded-md px-2 ml-3">Copy</button>
+                  </div>
+                  <button className="ml-20 font-semibold border w-fit p-1 border-black rounded-xl ">Generate Password</button>
+           </div>
+      </main>
     </>
   )
 }
